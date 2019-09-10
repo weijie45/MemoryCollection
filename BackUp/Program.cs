6856136d94e957ef1f6dfddb8e417ea1b464ca56
walter <Walter@u-gee.com>
2019-09-10 17:40:26 +0800

    2

--- BackUp/Program.cs
+++ BackUp/Program.cs
@@ -36,22 +36,28 @@ namespace BackUp
             if (!File.Exists(Path.Combine(_Destination, "Init.txt"))) {
                 Log($"{_Source} 同步至 {_Destination}");
                 Log("檔案同步中...");
+
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
                 DirectoryCopy(_Source, _Destination, true);
                 sw.Stop();
-                Log("檔案同步完成...");
 
                 var ts = sw.Elapsed;
                 Msg.AppendLine($"{Emoji.shinyStar}總耗時{string.Format("{0}:{1}", Math.Floor(ts.TotalMinutes), ts.ToString("ss\\.ff"))}");
                 LineSend(Msg.ToString());
-                Log($"總耗時{string.Format("{0}:{1}", Math.Floor(ts.TotalMinutes), ts.ToString("ss\\.ff"))}");
+
+                Log("檔案同步完成...");
+                Log($"  總耗時{string.Format("{0}:{1}", Math.Floor(ts.TotalMinutes), ts.ToString("ss\\.ff"))}");
 
                 File.CreateText(Path.Combine(_Destination, "Init.txt"));
             }
 
             Console.WriteLine("資料夾監測中...");
-            WatcherStrat(_Source, "");
+            try {
+                WatcherStrat(_Source, "");
+            } catch (Exception e) {
+                LineSend(e.Message);
+            }
 
             bool isContinue = true;
             while (isContinue) {
@@ -60,9 +66,40 @@ namespace BackUp
                 if (input.Equals("999", StringComparison.CurrentCultureIgnoreCase))
                     isContinue = false;
             }
+
+            SetConsoleCtrlHandler(t =>
+            {
+                // 這邊簡單的顯示導致視窗關閉的來源是什麼
+                Log(t.ToString());
+                Log("123");
+                // 在這裡處理視窗關閉前想要執行的程式碼
+                Console.WriteLine("222");
+                // 返回 false 將事件交回原處理函式執行正常關閉
+                return false;
+            },
+           true);
+
+            Console.WriteLine("按下任意鍵結束程式");
+
             Console.ReadKey();
 
+            Console.WriteLine("程式正常結束");
         }
+
+        [DllImport("Kernel32")]
+        public static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate handler, bool add);
+
+        public delegate bool ConsoleCtrlDelegate(CtrlTypes ctrlType);
+
+        public enum CtrlTypes
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
 
         private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
         {
@@ -112,7 +149,7 @@ namespace BackUp
                     }
 
                     if ((i > 1 && i % 50 == 0 && i != files.Count() - 1) || i == files.Count() - 1) {
-                        Log($"已複製{copyFileCnt}/{files.Count()}張... 耗時: {string.Format("{0}:{1}", Math.Floor(sw.Elapsed.TotalMinutes), sw.Elapsed.ToString("ss\\.ff"))}");
+                        Log($"\t已複製{copyFileCnt}/{files.Count()}張... 耗時: {string.Format("{0}:{1}", Math.Floor(sw.Elapsed.TotalMinutes), sw.Elapsed.ToString("ss\\.ff"))}");
                     }
                 }
                 sw.Stop();
@@ -125,7 +162,7 @@ namespace BackUp
                 }
 
                 //Log($"完成... 結果: {copyFileCnt}/{files.Count()}張");
-                Log($"耗時{string.Format("{0}:{1}", Math.Floor(ts.TotalMinutes), ts.ToString("ss\\.ff"))}");
+                Log($"\t耗時{string.Format("{0}:{1}", Math.Floor(ts.TotalMinutes), ts.ToString("ss\\.ff"))}");
 
                 // If copying subdirectories, copy them and their contents to new location.
                 if (copySubDirs) {
@@ -195,19 +232,48 @@ namespace BackUp
             }
         }
 
+        public static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            } catch (IOException) {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            } finally {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
         private static void WatcherStrat(string path, string filter)
         {
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = path;
-            watcher.IncludeSubdirectories = true;
-            //watcher.Filter = filter;
+            MyFileSystemWather myWather = new MyFileSystemWather(path, filter);
 
-            watcher.Changed += new FileSystemEventHandler(OnProcess);
-            watcher.Created += new FileSystemEventHandler(OnProcess);
-            watcher.Deleted += new FileSystemEventHandler(OnProcess);
-            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            myWather.OnChanged += new FileSystemEventHandler(OnChanged);
+            myWather.OnCreated += new FileSystemEventHandler(OnCreated);
+            myWather.OnRenamed += new RenamedEventHandler(OnRenamed);
+            myWather.OnDeleted += new FileSystemEventHandler(OnDeleted);
+            myWather.Start();
 
-            watcher.EnableRaisingEvents = true;
+            //FileSystemWatcher watcher = new FileSystemWatcher();
+            //watcher.Path = path;
+            //watcher.IncludeSubdirectories = true;
+            ////watcher.Filter = filter;
+
+            //watcher.Changed += new FileSystemEventHandler(OnProcess);
+            //watcher.Created += new FileSystemEventHandler(OnProcess);
+            //watcher.Deleted += new FileSystemEventHandler(OnProcess);
+            //watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            //watcher.EnableRaisingEvents = true;
         }
 
         private static void OnProcess(object source, FileSystemEventArgs e)
@@ -226,7 +292,7 @@ namespace BackUp
 
         private static void OnCreated(object source, FileSystemEventArgs e)
         {
-            Log($"--- 新增檔案 {DateTime.Now.ToString("yyyy/MM/dd")} ---");
+            Log($">>> 新增檔案 {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} ");
             var filePath = e.FullPath.Substring(e.FullPath.IndexOf("\\Upload")).Substring(7);
             var destPath = $"{_Destination}{filePath}";
 
@@ -235,15 +301,20 @@ namespace BackUp
             } catch (Exception) {
 
             }
+            try {
+                while (!IsFileLocked(new FileInfo(e.FullPath))) {
+                    File.Copy(e.FullPath, destPath);
 
-            File.Copy(e.FullPath, destPath);
+                    if (File.Exists(destPath)) {
+                        Log($"  成功 ! {destPath}");
+                    } else {
+                        Log($"  失敗 ! {e.FullPath}");
+                    }
 
-            if (File.Exists(destPath)) {
-                Log($"新增成功並同步 ! {destPath}");
-            } else {
-                Log($"新增失敗 ! {e.FullPath}");
+                }
+            } catch (Exception e1) {
+                LineSend(e1.Message);
             }
-
         }
 
         private static void OnChanged(object source, FileSystemEventArgs e)
@@ -265,14 +336,15 @@ namespace BackUp
 
             }
 
-            Log($"--- 刪除檔案 {DateTime.Now.ToString("yyyy/MM/dd")} ---");
+            Log($">> 刪除檔案 {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} ---");
 
-            Log($"備份刪除檔案...至 {backupPath}");
+            Log($"  備份刪除檔案...至 {backupPath}");
             File.Copy(destPath, backupPath);
+            Log(File.Exists(destPath) ? "  備份成功 !" : "");
 
             File.Delete(destPath);
 
-            Log(File.Exists(destPath) ? "" : $"刪除檔案並同步 !");
+            Log(File.Exists(destPath) ? "" : "  刪除成功 !");
         }
 
         private static void OnRenamed(object source, RenamedEventArgs e)
@@ -281,19 +353,19 @@ namespace BackUp
             var oldFilePath = e.OldFullPath.Substring(e.OldFullPath.IndexOf("\\Upload")).Substring(7);
             var sourcePath = e.FullPath;
             var destPath = $"{_Destination}{oldFilePath}";
-            Log($"--- 檔名變動 {DateTime.Now.ToString("yyyy/MM/dd")} ---");
+            Log($">> 檔名變動 {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} ---");
 
             File.Delete(destPath);
 
-            Log(File.Exists(destPath) ? "" : $"目的檔案已刪除 !");
+            Log(File.Exists(destPath) ? "" : "  目的檔案已刪除 !");
 
             destPath = $"{_Destination}{filePath}";
 
-            Log($"{sourcePath} 複製到 {destPath}...");
+            Log($"  {sourcePath} 複製到 {destPath}...");
 
             File.Copy(sourcePath, destPath, true);
 
-            Log(File.Exists(destPath) ? "複製檔案並同步  !" : "");
+            Log(File.Exists(destPath) ? "  改名成功  !" : "");
         }
 
 
